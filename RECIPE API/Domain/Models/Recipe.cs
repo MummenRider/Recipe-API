f533b6190a443853ed3cefd9490150aa83d26a5f
Inocalla, Clark eHS <Clark.Inocalla@eHealthsask.ca>
2020-03-09 15:44:23 -0600

    Add project files.

---
+++ RECIPE API/Domain/Models/Recipe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RECIPE_API.Domain.Models
+{
+    public class Recipe
+    {
+        public int RecipeId { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string ImageUrl { get; set; }
+        public IList<string> Ingridients { get; set; }
+
+        //FK
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ RECIPE API/Persistence/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RECIPE_API.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RECIPE_API.Persistence
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+        }
+    }
+}
