742684454484cd872eac997c341a6eb8410bd539
Clark Inocalla <clark.inocalla@hotmail.com>
2020-03-11 09:06:16 -0700

    Added  category responses for success and fail request.

--- RECIPE API/Domain/Services/ICategoryService.cs
+++ RECIPE API/Domain/Services/ICategoryService.cs
@@ -1,4 +1,5 @@
 ﻿using RECIPE_API.Domain.Models;
+using RECIPE_API.Domain.Services.Responses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,6 @@ namespace RECIPE_API.Domain.Services
     public interface ICategoryService
     {
         Task<IEnumerable<Category>> ListAsync();
+        Task<CategoryResponse> AddCategoryAsync(Category category);
     }
 }
---
+++ RECIPE API/Domain/Services/Responses/CategoryResponse.cs
@@ -0,0 +1,29 @@
+﻿using RECIPE_API.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RECIPE_API.Domain.Services.Responses
+{
+    public class CategoryResponse : BaseResponse
+    {
+        public Category Category { get; private set; }
+        /// <summary>
+        /// This will pass the parameters to base response
+        /// </summary>
+        /// <param name="success"></param>
+        /// <param name="message"></param>
+        /// <param name="category"></param>
+        private CategoryResponse(bool success, string message, Category category) : base(success, message)
+        {
+            Category = category;
+        }
+
+        public CategoryResponse(Category category) : this (true, String.Empty, category) { }
+        public CategoryResponse(string message) : this (false, message, null) { }
+  
+
+
+    }
+}
