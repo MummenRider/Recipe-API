f7bf7d1add55bb772ed4a2c413373b71bb2221ae
Clark Inocalla <clark.inocalla@hotmail.com>
2020-03-16 17:26:56 -0600

    Introduced add recipe in the project

--- RECIPE API/Domain/Services/IRecipeService.cs
+++ RECIPE API/Domain/Services/IRecipeService.cs
@@ -1,4 +1,5 @@
 ﻿using RECIPE_API.Domain.Models;
+using RECIPE_API.Domain.Services.Responses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,6 @@ namespace RECIPE_API.Domain.Services
     public interface IRecipeService
     {
         Task<IEnumerable<Recipe>> ListAsync();
+        Task<RecipeResponse> AddAsync(Recipe recipe);
     }
 }
---
+++ RECIPE API/Domain/Services/Responses/RecipeResponse.cs
@@ -0,0 +1,20 @@
+﻿using RECIPE_API.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RECIPE_API.Domain.Services.Responses
+{
+    public class RecipeResponse : BaseResponse
+    {
+        public Recipe Recipe { get; private set; }
+
+        private RecipeResponse(bool success, string error, Recipe recipe) : base(success, error) { Recipe = recipe; }
+
+        public RecipeResponse(Recipe recipe) : this(true, String.Empty, recipe) { }
+        public RecipeResponse(string error) : this(true, error, null) { }
+
+  
+    }
+}
