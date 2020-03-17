98bc7886b69c4b254ef16f8f87638a04ded2accd
Clark Inocalla <clark.inocalla@hotmail.com>
2020-03-16 23:27:18 -0600

    Made the recipe properties required

--- RECIPE API/DataTransferObjects/SaveRecipeDto.cs
+++ RECIPE API/DataTransferObjects/SaveRecipeDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,15 @@ namespace RECIPE_API.DataTransferObjects
 {
     public class SaveRecipeDto
     {
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Summary { get; set; }
+        [Required]
         public string ImageUrl { get; set; }
+        [Required]
         public IList<string> Ingridients { get; set; }
+        [Required]
         public int CategoryId { get; set; }
     }
 }
