749942db0f144214a09659561575e40077cfec18
Inocalla, Clark eHS <Clark.Inocalla@eHealthsask.ca>
2020-03-23 16:05:09 -0600

    Added User model class in the project. I shall finish the User Role model class for the next commit

---
+++ RECIPE API/Domain/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RECIPE_API.Domain.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        //Collection of User role (Admind / Regular) user
+        public ICollection<UserRole> UserRoles { get; set; } = new Collection<UserRole>();
+    }
+}
