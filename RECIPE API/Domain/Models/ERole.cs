8b44a8c9e7ecfb101a26be7281a3a771e8ec4333
Inocalla, Clark eHS <Clark.Inocalla@eHealthsask.ca>
2020-03-24 09:13:33 -0600

    Added enum for role

---
+++ RECIPE API/Domain/Models/ERole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RECIPE_API.Domain.Models
+{
+    public enum ERole
+    {
+        Regular = 1,
+        Admin   = 2
+    }
+}
