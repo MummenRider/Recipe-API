862e3b66cdb336928a04eba352f5ff00aac5a799
Inocalla, Clark eHS <Clark.Inocalla@eHealthsask.ca>
2020-03-25 10:12:51 -0600

    Added a user response wheneve a user has been created

---
+++ RECIPE API/Domain/Services/Responses/UserResponse.cs
@@ -0,0 +1,15 @@
+﻿using RECIPE_API.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RECIPE_API.Domain.Services.Responses
+{
+    public class UserResponse : BaseResponse<User>
+    {
+        public UserResponse(User user) : base(user) { }
+        public UserResponse(string message) : base(message) { }
+
+    }
+}
