62bb5801bbf9d20c99a6a550e1b854f0c9ef2f1e
Clark Inocalla <clark.inocalla@hotmail.com>
2020-03-27 12:04:18 -0600

    added interface for the user service and an empty implementation of it.

---
+++ RECIPE API/Domain/Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using RECIPE_API.Domain.Models;
+using RECIPE_API.Domain.Services.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RECIPE_API.Domain.Services
+{
+    public interface IUserService
+    {
+        Task<UserResponse> AddUserAsync(User user);
+    }
+}
